bd3a9b30d2fd4ce8021e29fc3a9af30049cbb6ef
Hudson Tadeu <hudsontadeu2010@gmail.com>
2024-04-16 10:20:21 -0300

    Initial Commit

---
+++ src/components/login/Login.js
@@ -0,0 +1,104 @@
+import React, { useState } from "react";
+import "./styleLogin.css";
+import { useNavigate } from "react-router-dom";
+
+function Login() {
+  const navigate = useNavigate();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+
+    try {
+      const dataToSend = {
+        user: username,
+        password: password,
+      };
+
+      const base64Credentials = btoa(`${username}:${password}`);
+      const response = await fetch(
+        "http://131.161.43.14:8280/dts/datasul-rest/resources/prg/etq/v1/piGetLogin",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Basic ${base64Credentials}`,
+          },
+          body: JSON.stringify(dataToSend),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Erro ao autenticar usuário");
+      }
+
+      const responseData = await response.json();
+
+      if (
+        responseData.total === 1 &&
+        responseData.items &&
+        responseData.items.length > 0 &&
+        responseData.items[0].Erro === 0
+      ) {
+        sessionStorage.setItem("token", base64Credentials);
+        sessionStorage.setItem("username", username);
+        navigate("/menu");
+      } else {
+        throw new Error("Usuário ou senha incorretos");
+      }
+    } catch (error) {
+      setError(error.message);
+      console.error("Erro ao efetuar login:", error);
+    }
+  };
+
+  return (
+    <div className="mainLogin">
+      <div className="logo">
+        <img
+          src="conti_original-fundo-branco.jpg"
+          className="logoImage"
+          alt="ConTi Logo"
+        />
+      </div>
+      <div className="cardLogin">
+        <form onSubmit={handleLogin}>
+          <div className="formGroup">
+            <label htmlFor="username">Usuário:</label>
+            <input
+              type="text"
+              className="input"
+              id="username"
+              name="username"
+              placeholder="Usuário"
+              value={username}
+              onChange={(event) => setUsername(event.target.value)}
+            />
+          </div>
+          <div className="textfield">
+            <div className="formGroup">
+              <label htmlFor="password">Senha:</label>
+              <input
+                type="password"
+                className="input"
+                id="password"
+                name="password"
+                placeholder="Senha"
+                value={password}
+                onChange={(event) => setPassword(event.target.value)}
+              />
+            </div>
+          </div>
+          <button type="submit" className="btnLogin">
+            Entrar
+          </button>
+        </form>
+      </div>
+      {error && <p>{error}</p>}
+    </div>
+  );
+}
+
+export default Login;
