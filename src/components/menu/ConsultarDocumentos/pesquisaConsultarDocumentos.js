4563b69900dbf71231530529af22e0e63cc8906c
Diego Henrique <dieghrp20@gmail.com>
2024-05-15 20:36:50 -0300

    fix: Alteração no layout de consultar documento

--- src/components/menu/ConsultarDocumentos/pesquisaConsultarDocumentos.js
+++ src/components/menu/ConsultarDocumentos/pesquisaConsultarDocumentos.js
@@ -1,38 +1,67 @@
 import React from 'react';
 import './stylePesquisaConsultarDocumentos.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { faSearch, faPencilAlt } from '@fortawesome/free-solid-svg-icons';
 
 function PesquisaConsultarDocumentos() {
+  // Status: 1 - pendente 2 - atualizado 3 - cancelado
   const documentos = [
     {
       fornecedor: 'Fornecedor 1',
       documento: 'Documento 1',
       serie: 'Série 1',
       natureza_operacao: 'Natureza 1',
-      data_emissao: 'Data 1'
+      data_emissao: '01/03/2024',
+      status: 1
     },
     {
       fornecedor: 'Fornecedor 2',
       documento: 'Documento 2',
       serie: 'Série 2',
       natureza_operacao: 'Natureza 2',
-      data_emissao: 'Data 2'
+      data_emissao: '02/03/2024',
+      status: 2
     },
     {
       fornecedor: 'Fornecedor 3',
       documento: 'Documento 3',
       serie: 'Série 3',
       natureza_operacao: 'Natureza 3',
-      data_emissao: 'Data 3'
+      data_emissao: '03/03/2024',
+      status: 3
     }
   ];
+
+  const getStatusInfo = (status) => {
+    let statusText;
+    let statusColor;
+    switch (status) {
+      case 1:
+        statusText = 'Pendente';
+        statusColor = '#FFD700'; // Amarelo escuro
+        break;
+      case 2:
+        statusText = 'Atualizado';
+        statusColor = '#008000'; // Verde
+        break;
+      case 3:
+        statusText = 'Cancelado';
+        statusColor = '#FF0000'; // Vermelho
+        break;
+      default:
+        statusText = 'Status Desconhecido';
+        statusColor = '#808080'; // Cinza
+        break;
+    }
+    return { text: statusText, color: statusColor };
+  };
 
   return (
     <div className="body-pesquisaConsultarDocumentos">
       <div className="container-pesquisaConsultarDocumentos">
         <h1 className="title-pesquisaConsultarDocumentos">Consultar Documentos</h1>
         <div className="controls-container-pesquisaConsultarDocumentos">
+          <button className="button-primary-pcd">Outras Opções</button>
           <div className="search-container-pcd">
             <input
               type="text"
@@ -43,25 +72,45 @@ function PesquisaConsultarDocumentos() {
               <FontAwesomeIcon icon={faSearch} />
             </button>
           </div>
-          <button className="button-primary-pcd">Outras Opções</button>
         </div>
         <div className="lista-documentos-pcd">
-          {documentos.map((documento, index) => (
-            <div className="documento-item-pcd" key={index}>
-                 <div>
-              <p>Fornecedor: {documento.fornecedor}</p>
-              <p>Documento: {documento.documento}</p>
+          {documentos.map((documento, index) => {
+            const statusInfo = getStatusInfo(documento.status);
+            return (
+              <div className="documento-item-pcd" key={index}>
+                <div className="documento-titulo-pcd">
+                  <p>{documento.documento.toUpperCase()}</p>
+                </div>
+                <div className="documento-detalhes-pcd">
+                  <div className="documento-status-fornecedor-pcd">
+                    <div className="status-container">
+                      <span className="status-circle" style={{ backgroundColor: statusInfo.color }}>{documento.status}</span>
+                      <div className="status-description" style={{ backgroundColor: statusInfo.color }}>{statusInfo.text}</div>
+                    </div>
+                    <p>{documento.fornecedor}</p>
+                  </div>
+                  <div className="documento-serie-natureza-pcd">
+                    <p><strong>Série:</strong> {documento.serie}</p>
+                    <p><strong>Natureza de Operação:</strong> {documento.natureza_operacao}</p>
+                  </div>
+                  <div className="documento-data-pcd">
+                    <p><strong>Data de Emissão:</strong> {documento.data_emissao}</p>
+                  </div>
+                  <div className="documento-acoes-pcd">
+                    <FontAwesomeIcon icon={faPencilAlt} title="Editar" className="icon-edit" />
+                    <div className="dropdown" title="Outras opções">
+                      <button className="dropbtn">...</button>
+                      <div className="dropdown-content">
+                        <a href="#">Opção 1</a>
+                        <a href="#">Opção 2</a>
+                        <a href="#">Opção 3</a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
-              <div className="documento-item-sub-pcd">
-              <p>Série: {documento.serie}</p>
-              <p>Natureza de Operação: {documento.natureza_operacao}</p>
-              <p>Data de Emissão: {documento.data_emissao}</p>
-              <div>
-              <button className="button-secondary-pcd">Outras Opções</button>
-              </div>
-            </div>
-            </div>
-          ))}
+            );
+          })}
         </div>
       </div>
     </div>
