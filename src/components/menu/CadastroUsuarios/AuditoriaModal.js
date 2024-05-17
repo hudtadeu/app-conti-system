e7eb5dfe915a9866af08c9b8d179c0a76eb64422
Hudson Tadeu <hudsontadeu2010@gmail.com>
2024-05-17 15:16:20 -0300

    Atualização

--- src/components/menu/CadastroUsuarios/AuditoriaModal.js
+++ src/components/menu/CadastroUsuarios/AuditoriaModal.js
@@ -9,22 +9,12 @@ const AuditoriaModal = ({ onClose, user }) => {
       <div className="modal-audit-usuario">
         <div className="modal-audit-user">
           <h2 className="title-audit">
-            Acesso a Auditoria
+            Acesso ao Painel
             <button onClick={onClose} className="close-button-audit">
               &times;
             </button>
           </h2>
           <div className="quadrante-title-audit">
-            <p>
-              Usuário: <span class="user-audit">{user["cod-usuario"]}</span>
-            </p>
-            <p>
-              Estabelecimento:{" "}
-              <span class="user-audit">{user["cod-estabel"]}</span>
-            </p>
-          </div>
-          <div className="quadrante-title-audit">
-            <h4 className="painel-title-audit">Acesso ao Painel</h4>
             <div className="audit-container">
             <span className="subtitle-audit">
               <button className="toggle-button-audit">
