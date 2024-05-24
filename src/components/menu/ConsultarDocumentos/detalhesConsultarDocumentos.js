f9668772d7c669033ce26e30991ac94e2090a743
Hudson Tadeu <hudsontadeu2010@gmail.com>
2024-05-24 10:11:54 -0300

    Atualização

--- src/components/menu/ConsultarDocumentos/detalhesConsultarDocumentos.js
+++ src/components/menu/ConsultarDocumentos/detalhesConsultarDocumentos.js
@@ -3,18 +3,19 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronDown, faChevronRight } from '@fortawesome/free-solid-svg-icons';
 import { useLocation } from 'react-router-dom';
 import "./styleDetalhesConsultarDocumentos.css";
-import { getStatusInfo } from '../../utils';
+import { getStatusInfo, getTipoDocumentoInfo } from '../../utils';
 
 function DetalhesConsultarDocumentos() {
   const location = useLocation();
   const { cId } = location.state;
   const [documento, setDocumento] = useState(null);
   const [sections, setSections] = useState({
-    manutencao: false,
-    datas: false,
-    contabilizacao: false,
-    documentacao: false,
-    assinatura: false
+    detalhes: false,
+    valores: false,
+    valoresb: false,
+    observacao: false,
+    transp: false,
+    nfe: false
   });
   const [activeButton, setActiveButton] = useState('');
 
@@ -74,7 +75,8 @@ function DetalhesConsultarDocumentos() {
   if (!documento) {
     return <div>Carregando...</div>;
   }
-
+  const statusInfo = getStatusInfo(documento.situacao);
+  const tipoDocumentoInfo = getTipoDocumentoInfo(documento.tipo_doc);
   return (
     <div className="body-detailsdoc">
       <div className="container-detailsdoc">
@@ -82,48 +84,51 @@ function DetalhesConsultarDocumentos() {
           <h1 className="title-detailsdoc">Detalhes do Documento</h1>
           <div className="document-info-dcd">
             <div className="document-title-dcd">
-              <p><strong>Documento:</strong> {documento.nro_docto}</p>
+              <p> {documento.nro_docto}</p>
             </div>
-            <div className="document-details-dcd">
-              <div>
-                <p><strong>Status:</strong> {getStatusInfo(documento.situacao).text}</p>
-                <p><strong>Fornecedor:</strong> {documento.forneced}</p>
-              </div>
-              <div className="documento-serie-natureza-pcd">
-                <p><strong>Série:</strong> {documento.serie_docto}</p>
-                <p><strong>Natureza de Operação:</strong> {documento.nat_operacao}</p>
-              </div>
-              <div className="documento-data-pcd">
-                <p><strong>Data de Emissão:</strong> {formatDate(documento.emissao)}</p>
-              </div>
+            <div className="documento-detalhes-pcd">
+                  <div className="documento-status-fornecedor-pcd">
+                    <div className="status-container">
+                      <div className="status-description" style={{ backgroundColor: statusInfo.color }}>{statusInfo.text}</div>
+                      <div className="tipo-container">
+                        <div className="tipo-description" style={{ backgroundColor: tipoDocumentoInfo.color }}>{tipoDocumentoInfo.text}</div>
+                      </div>
+                    </div>
+                    <p className='title-forneced-pcd'>{documento.forneced}</p>
+                  </div>
+                  <div className="documento-serie-natureza-pcd">
+                    <p><strong>Série:</strong> {documento.serie_docto}</p>
+                    <p><strong>Natureza da Operação:</strong> {documento.nat_operacao}</p>
+                  </div>
+                  <div className="documento-data-pcd">
+                    <p><strong>Data de Emissão:</strong> {formatDate(documento.emissao)}</p>
+                  </div>
             </div>
           </div>
-          <div className='conteudoDetalhes' >
+          
           <div className="section-detailsdoc" onClick={() => toggleSection('detalhes')}>
             <h2>Detalhes <FontAwesomeIcon icon={sections.detalhes ? faChevronDown : faChevronRight} /></h2>
             <div className={`section-content-detailsdoc ${sections.detalhes ? 'show' : ''}`}>
-            <p><strong>Fornecedor:</strong> {documento.forneced}</p>
-            <p><strong>Série:</strong> {documento.serie_docto} {documento.arquivo_atualizado && "Arquivo Atualizado no EMS"}</p>
-            <p><strong>Natureza da Operação:</strong> {documento.nat_operacao} </p>
-            <p><strong>Observação:</strong> {documento.cod_observa}</p>
-            <p><strong>Estabelecimento:</strong> {documento.estabel}</p>
-            <p><strong>Data da Transação:</strong> {documento.dt_trans}<span> Usuário Atualiza: {documento.usuario_atualiza}</span></p>
-            <p><strong>Tipo de Documento:</strong> {documento.tipo_doc}</p>
-            <p><strong>Data de Importação:</strong> {documento.dt_importa}<span> Usuário de Importação: {documento.cod_usuario_importa}</span>
-            <span><strong>Data de Emissão:</strong> {documento.emissao}</span></p>
-            <p><strong>Chave do Documento:</strong> {documento.chave_documento}</p>
-            <p><strong>Protocolo:</strong> {documento.protocolo}</p>
-            <p><strong>Situação do Manifesto:</strong> {documento.situacao_manifesto}</p>
-            <p><strong>Protocolo Cancelamento:</strong> {documento.protocolo_cancel}
-            <span><strong>Usuário Cancelamento:</strong> {documento.cod_usuario_cancel}
-            <span><strong>Data de Cancelamento:</strong> {documento.dt_cancel_doc}</span></span></p>
-              </div>
-              </div>
+              <p><strong>Fornecedor:</strong> {documento.forneced}</p>
+              <p><strong>Série:</strong> {documento.serie_docto} {documento.arquivo_atualizado && "Arquivo Atualizado no EMS"}</p>
+              <p><strong>Natureza da Operação:</strong> {documento.nat_operacao}</p>
+              <p><strong>Observação:</strong> {documento.cod_observa}</p>
+              <p><strong>Estabelecimento:</strong> {documento.estabel}</p>
+              <p><strong>Data da Transação:</strong> {documento.dt_trans}<span> Usuário Atualiza: {documento.usuario_atualiza}</span></p>
+              <p><strong>Tipo de Documento:</strong> {documento.tipo_doc}</p>
+              <p><strong>Data de Importação:</strong> {documento.dt_importa}<span> Usuário de Importação: {documento.cod_usuario_importa}</span></p>
+              <p><strong>Data de Emissão:</strong> {documento.emissao}</p>
+              <p><strong>Chave do Documento:</strong> {documento.chave_documento}</p>
+              <p><strong>Protocolo:</strong> {documento.protocolo}</p>
+              <p><strong>Situação do Manifesto:</strong> {documento.situacao_manifesto}</p>
+              <p><strong>Protocolo Cancelamento:</strong> {documento.protocolo_cancel}
+              <span><strong>Usuário Cancelamento:</strong> {documento.cod_usuario_cancel}
+              <span><strong>Data de Cancelamento:</strong> {documento.dt_cancel_doc}</span></span></p>
+            </div>
           </div>
-          
-          <div className='conteudoValores'>
+
           <div className="section-detailsdoc" onClick={() => toggleSection('valores')}>
-            <h2>Valores  <FontAwesomeIcon icon={sections.valores ? faChevronDown : faChevronRight} /></h2>
+            <h2>Valores <FontAwesomeIcon icon={sections.valores ? faChevronDown : faChevronRight} /></h2>
             <div className={`section-content-detailsdoc ${sections.valores ? 'show' : ''}`}>
               <p><strong>Valor Frete:</strong> {documento["valor-frete"]}
               <span><strong>Peso Total:</strong> {documento["tot-peso"]}</span></p>
@@ -144,13 +149,11 @@ function DetalhesConsultarDocumentos() {
               <span><strong>Volume:</strong> {documento["volume"]}</span></p>
             </div>
           </div>
-          </div>
 
-          <div className='conteudoValoresB'>
           <div className="section-detailsdoc" onClick={() => toggleSection('valoresb')}>
             <h2>Valores || <FontAwesomeIcon icon={sections.valoresb ? faChevronDown : faChevronRight} /></h2>
             <div className={`section-content-detailsdoc ${sections.valoresb ? 'show' : ''}`}>
-            <p><strong>PIS Retido:</strong> {documento["vl-pis-ret"]}</p>
+              <p><strong>PIS Retido:</strong> {documento["vl-pis-ret"]}</p>
               <p><strong>COFINS Retido:</strong> {documento["vl-cofins-ret"]}</p>
               <p><strong>CSLL Retido:</strong> {documento["vl-csll-ret"]}</p>
               <p><strong>Base IRRF:</strong> {documento["vl-base-irrf"]}</p>
@@ -164,27 +167,24 @@ function DetalhesConsultarDocumentos() {
               <p><strong>Valor COFINS Efetivo:</strong> {documento["vl-cofins-efet"]}</p>
               <p><strong>Valor PIS:</strong> {documento["vl-pis"]}</p>
               <p><strong>Valor PIS Efetivo:</strong> {documento["vl-pis-efet"]}</p>
-              <div className='quadrante-ec'><span>EC 87</span>
-              <p><strong>Valor Aprox  Trib:</strong> {documento[""]}</p>
-              <p><strong>Valor ICMS Deson:</strong> {documento["vl-icms-deson"]}</p>
-              <p><strong>Valor ICMS FCP Dest:</strong> {documento["vl-fcp-uf-dest"]}</p>
-              <p><strong>Valor ICMS UF Dest:</strong> {documento["vl-icms-uf-dest"]}</p>
-              <p><strong>Valor ICMS UF Rem:</strong> {documento["vl-icms-uf-rem"]}</p>
+              <div className='quadrante-ec'>
+                <h5>EC 87</h5>
+                <p><strong>Valor Aprox Trib:</strong> {documento["vl-aprox-trib"]}</p>
+                <p><strong>Valor ICMS Deson:</strong> {documento["vl-icms-deson"]}</p>
+                <p><strong>Valor ICMS FCP Dest:</strong> {documento["vl-fcp-uf-dest"]}</p>
+                <p><strong>Valor ICMS UF Dest:</strong> {documento["vl-icms-uf-dest"]}</p>
+                <p><strong>Valor ICMS UF Rem:</strong> {documento["vl-icms-uf-rem"]}</p>
               </div>
             </div>
           </div>
-          </div>
 
-          <div>
           <div className="section-detailsdoc" onClick={() => toggleSection('observacao')}>
             <h2>Observação <FontAwesomeIcon icon={sections.observacao ? faChevronDown : faChevronRight} /></h2>
             <div className={`section-content-detailsdoc ${sections.observacao ? 'show' : ''}`}>
-            <p> {documento["inf-complement"]}</p>
-              </div>
-          </div>
+              <p> {documento["inf-complement"]}</p>
+            </div>
           </div>
 
-          <div className='conteudoTransp'>
           <div className="section-detailsdoc" onClick={() => toggleSection('transp')}>
             <h2>Transp <FontAwesomeIcon icon={sections.transp ? faChevronDown : faChevronRight} /></h2>
             <div className={`section-content-detailsdoc ${sections.transp ? 'show' : ''}`}>
@@ -199,17 +199,15 @@ function DetalhesConsultarDocumentos() {
               <p><strong>Modalid. Frete:</strong> {documento["mod-frete"]}</p>
               <p><strong>Tipo CT-e:</strong> {documento[""]}</p>
               <p><strong>UF Orig/Dest(GIA-SP):</strong> {documento["uf-dest"]}</p>
-              <p><strong>Cód IBGE Munic Orig:</strong> {documento["municipio-entrega"]}
-              <p><strong>Cód IBGE Munic Dest:</strong> {documento["municipio-retirada"]}</p></p>
-              </div>
-          </div>
+              <p><strong>Cód IBGE Munic Orig:</strong> {documento["municipio-entrega"]}</p>
+              <p><strong>Cód IBGE Munic Dest:</strong> {documento["municipio-retirada"]}</p>
+            </div>
           </div>
 
-          <div className='conteudoNfe'>
           <div className="section-detailsdoc" onClick={() => toggleSection('nfe')}>
             <h2>NF3e <FontAwesomeIcon icon={sections.nfe ? faChevronDown : faChevronRight} /></h2>
             <div className={`section-content-detailsdoc ${sections.nfe ? 'show' : ''}`}> 
-            <p><strong>Consumo Energ Elét/Gás/Água Canaliz:</strong> {documento[""]}</p>
+              <p><strong>Consumo Energ Elét/Gás/Água Canaliz:</strong> {documento[""]}</p>
               <p><strong>Valor Total Fornecido/Consumido:</strong> {documento[""]}</p>
               <p><strong>Serviços Não-tributados Pelo ICMS:</strong> {documento["icm-nao-trib"]}</p>
               <p><strong>Vl Total Cobrado Nome de Terceiros:</strong> {documento[""]}</p>
@@ -218,8 +216,7 @@ function DetalhesConsultarDocumentos() {
               <p><strong>Indicador Destinatário/Acessante:</strong> {documento[""]}</p>
               <p><strong>Energia Injetada:</strong> {documento[""]}</p>
               <p><strong>Outras Deduções:</strong> {documento[""]}</p>
-              </div>
-          </div>
+            </div>
           </div>
 
           <div className="button-bar">
@@ -236,8 +233,8 @@ function DetalhesConsultarDocumentos() {
               Valores
             </button>
             <button
-              className={activeButton === 'valoresII' ? 'active' : ''}
-              onClick={() => handleButtonClick('valoresII')}
+              className={activeButton === 'valoresb' ? 'active' : ''}
+              onClick={() => handleButtonClick('valoresb')}
             >
               Valores||
             </button>
@@ -254,21 +251,20 @@ function DetalhesConsultarDocumentos() {
               Transp
             </button>
             <button
-              className={activeButton === 'nf3e' ? 'active' : ''}
-              onClick={() => handleButtonClick('nf3e')}
+              className={activeButton === 'nfe' ? 'active' : ''}
+              onClick={() => handleButtonClick('nfe')}
             >
               NF3e
             </button>
           </div>
-          <div >
+
           <div className={`content-section ${activeButton === 'detalhes' ? 'show' : ''}`}>
             Conteúdo
-          </div>
           </div>
           <div className={`content-section ${activeButton === 'valores' ? 'show' : ''}`}>
             Conteúdo dos Valores
           </div>
-          <div className={`content-section ${activeButton === 'valoresII' ? 'show' : ''}`}>
+          <div className={`content-section ${activeButton === 'valoresb' ? 'show' : ''}`}>
             Conteúdo dos Valores||
           </div>
           <div className={`content-section ${activeButton === 'observacao' ? 'show' : ''}`}>
@@ -277,7 +273,7 @@ function DetalhesConsultarDocumentos() {
           <div className={`content-section ${activeButton === 'transp' ? 'show' : ''}`}>
             Conteúdo do Transporte
           </div>
-          <div className={`content-section ${activeButton === 'nf3e' ? 'show' : ''}`}>
+          <div className={`content-section ${activeButton === 'nfe' ? 'show' : ''}`}>
             Conteúdo do NF3e
           </div>
         </div>
