596193f5937e94042b625b16eec9d9ae06f400cf
Hudson Tadeu <hudsontadeu2010@gmail.com>
2024-06-03 09:58:21 -0300

    Atualização

--- src/components/menu/ConsultarDocumentos/ConsultarDocumentos.js
+++ src/components/menu/ConsultarDocumentos/ConsultarDocumentos.js
@@ -24,6 +24,7 @@ function ConsultarDocumentos() {
           field.value = savedFormData[fieldName];
         }
       });
+      
     } else {
       formRef.current.elements['tipoDocumento'].value = '';
     }
