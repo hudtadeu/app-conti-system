0a2151747c336421bbab1b2aaee5cfe64a5a179d
Hudson Tadeu <hudsontadeu2010@gmail.com>
2024-07-01 15:44:44 -0300

    Atualização

--- src/components/menu/relatorios/xmlObrigFiscais/XmlObrigFiscais.js
+++ src/components/menu/relatorios/xmlObrigFiscais/XmlObrigFiscais.js
@@ -2,50 +2,52 @@ import React, { useState } from 'react';
 import "./styleXmlObrigFiscais.css";
 
 const XmlObrigFiscais = () => {
-  const [formToShow, setFormToShow] = useState(null);
+  const [formToShow, setFormToShow] = useState('Seleção');
+  const [activeButton, setActiveButton] = useState('Seleção');
 
   const showForm = (formName) => {
     setFormToShow(formName);
+    setActiveButton(formName);
   };
 
   const renderForm = () => {
     switch (formToShow) {
       case 'Seleção':
         return (
-            <form className="form-selection">
+          <form className="form-selection">
             <label>
               <span>Estabelecimento:
-              <input type="text" readOnly />
-              até
-              <input type="text"  readOnly />
+                <input type="text" />
+                até
+                <input type="text" />
               </span>
             </label>
             <br />
             <label>
-              <span> Data de Emissão:
-              <input type="text"  readOnly />
-              até
-              <input type="text"  readOnly />
+              <span>Data de Emissão:
+                <input type="date" />
+                até
+                <input type="date" />
               </span>
             </label>
             <br />
             <label>
-              <span> Emitente:
-              <input type="text" readOnly />
-              até
-              <input type="text" readOnly />
+              <span>Emitente:
+                <input type="text" readOnly />
+                até
+                <input type="text" readOnly />
               </span>
             </label>
             <br />
             <label>
-              <span> Data de Transação:
-              <input type="text"  readOnly />
-              até
-              <input type="text" readOnly />
+              <span>Data de Transação:
+                <input type="date" />
+                até
+                <input type="date" />
               </span>
             </label>
-          <button className="submit-alterar-senha" type="submit">Executar</button>
-            <button className="button-alterar-senha" type="button">Cancelar</button>
+            <button className="submit-executar-fiscais" type="submit">Executar</button>
+            <button className="button-executar-fiscais" type="button">Cancelar</button>
           </form>
         );
       case 'Classificação':
@@ -74,10 +76,30 @@ const XmlObrigFiscais = () => {
   return (
     <div className="container-empresa">
       <div className="button-group">
-        <button className="button-secondary" onClick={() => showForm('Seleção')}>Seleção</button>
-        <button className="button-secondary" onClick={() => showForm('Classificação')}>Classificação</button>
-        <button className="button-secondary" onClick={() => showForm('Parâmetros')}>Parâmetros</button>
-        <button className="button-secondary" onClick={() => showForm('Impressão')}>Impressão</button>
+        <button
+          className={`button-secondary ${activeButton === 'Seleção' ? 'active' : ''}`}
+          onClick={() => showForm('Seleção')}
+        >
+          Seleção
+        </button>
+        <button
+          className={`button-secondary ${activeButton === 'Classificação' ? 'active' : ''}`}
+          onClick={() => showForm('Classificação')}
+        >
+          Classificação
+        </button>
+        <button
+          className={`button-secondary ${activeButton === 'Parâmetros' ? 'active' : ''}`}
+          onClick={() => showForm('Parâmetros')}
+        >
+          Parâmetros
+        </button>
+        <button
+          className={`button-secondary ${activeButton === 'Impressão' ? 'active' : ''}`}
+          onClick={() => showForm('Impressão')}
+        >
+          Impressão
+        </button>
       </div>
 
       {/* Renderiza o formulário selecionado */}
