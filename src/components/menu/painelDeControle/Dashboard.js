5d5bd9156268008a8716966df3c5a39e5a3163aa
Hudson Tadeu <hudsontadeu2010@gmail.com>
2024-07-10 11:55:38 -0300

    Atualização

--- src/components/menu/painelDeControle/Dashboard.js
+++ src/components/menu/painelDeControle/Dashboard.js
@@ -4,6 +4,7 @@ import Modal from 'react-modal';
 import { Line, Bar, Pie } from 'react-chartjs-2';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSyncAlt, faCog, faAngleDown, faBuilding } from '@fortawesome/free-solid-svg-icons';
+import ApexCharts from 'react-apexcharts';
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -29,11 +30,46 @@ ChartJS.register(
   Legend
 );
 
+const getTipoDocumentoInfo = (tipoDoc) => {
+  let tipoText;
+  let tipoColor;
+  switch (tipoDoc) {
+    case 'NF-e':
+      tipoText = 'NF-e';
+      tipoColor = '#32CD32';
+      break;
+    case 'CT-e':
+      tipoText = 'CT-e';
+      tipoColor = '#00BFFF';
+      break;
+    case 'CTe-OS':
+      tipoText = 'CTe-OS';
+      tipoColor = '#8A2BE2';
+      break;
+    case 'NFS-e':
+      tipoText = 'NFS-e';
+      tipoColor = '#FF8C00';
+      break;
+    case 'NF3e':
+      tipoText = 'NF3e';
+      tipoColor = '#FF1493';
+      break;
+    default:
+      tipoText = 'Diversos';
+      tipoColor = '#808080';
+      break;
+  }
+  return { text: tipoText, color: tipoColor };
+};
+
 const Dashboard = () => {
   const [lineData, setLineData] = useState(null);
   const [barData, setBarData] = useState(null);
   const [pieData, setPieData] = useState(null);
+  const [chartData, setChartData] = useState([]);
   const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [pieModalIsOpen, setPieModalIsOpen] = useState(false);
+  const [secondModalIsOpen, setSecondModalIsOpen] = useState(false);
   const [considerarDocumentos, setConsiderarDocumentos] = useState([]);
   const [faixasSelecao, setFaixasSelecao] = useState([]);
   const [graficosSelecao, setGraficosSelecao] = useState([]);
@@ -107,76 +143,86 @@ const Dashboard = () => {
     setBigNumbersSelecao(bigNumbersSelecao.filter(item => item !== option));
   };
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const base64Credentials = sessionStorage.getItem("token");
-      try {
-        const response = await fetch('http://131.161.43.14:8280/dts/datasul-rest/resources/prg/etq/v1/piGetDocumXML', {
-          method: "POST",
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Basic ${base64Credentials}`,
+  const fetchData = async () => {
+    const base64Credentials = sessionStorage.getItem("token");
+    try {
+      const response = await fetch('http://131.161.43.14:8280/dts/datasul-rest/resources/prg/etq/v1/piGetDocumXML/graph', {
+        method: "GET",
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Basic ${base64Credentials}`,
+        },
+      });
+      const data = await response.json();
+      const items = data.items;
+
+      const processedLineData = {
+        labels: items.map(item => item.emissao),
+        datasets: [
+          {
+            label: 'Documentos',
+            data: items.map(item => parseInt(item.nro_docto)),
+            fill: false,
+            backgroundColor: '#ebecee',
+            borderColor: '#ebecee',
+            tension: 0.4,
+            borderWidth: 2,
           },
-        });
-        const data = await response.json();
-        const items = data.items;
+        ],
+      };
 
-        const processedLineData = {
-          labels: items.map(item => item.emissao),
-          datasets: [
-            {
-              label: 'Documentos',
-              data: items.map(item => parseInt(item.nro_docto)),
-              fill: false,
-              backgroundColor: '#ebecee',
-              borderColor: '#ebecee',
-              tension: 0.4,
-              borderWidth: 2,
-            },
-          ],
-        };
+      const processedBarData = {
+        labels: items.map(item => item.emissao),
+        datasets: [
+          {
+            label: 'Documentos',
+            data: items.map(item => parseInt(item.nro_docto)),
+            backgroundColor: '#ebecee',
+            borderColor: '#ebecee',
+            borderWidth: 1,
+            borderRadius: 5,
+            barPercentage: 0.6,
+            categoryPercentage: 0.6,
+          },
+        ],
+      };
 
-        const processedBarData = {
-          labels: items.map(item => item.emissao),
-          datasets: [
-            {
-              label: 'Documentos',
-              data: items.map(item => parseInt(item.nro_docto)),
-              backgroundColor: '#ebecee',
-              borderColor: '#ebecee',
-              borderWidth: 1,
-              borderRadius: 5,
-              barPercentage: 0.6,
-              categoryPercentage: 0.6,
-            },
-          ],
-        };
+      const processedPieData = {
+        labels: ['Pendente', 'Atualizado', 'Cancelado'],
+        datasets: [
+          {
+            data: [
+              items.filter(item => item.situacao === 'Pendente').length,
+              items.filter(item => item.situacao === 'Atualizado').length,
+              items.filter(item => item.situacao === 'Cancelado').length,
+            ],
+            backgroundColor: ['#FFD700', '#008000', '#FF0000'],
+            hoverBackgroundColor: ['#FFD700', '#008000', '#FF0000'],
+            borderWidth: 1,
+            hoverOffset: 4,
+          },
+        ],
+      };
 
-        const processedPieData = {
-          labels: ['Pendente', 'Atualizado', 'Cancelado'],
-          datasets: [
-            {
-              data: [
-                items.filter(item => item.situacao === 'Pendente').length,
-                items.filter(item => item.situacao === 'Atualizado').length,
-                items.filter(item => item.situacao === 'Cancelado').length,
-              ],
-              backgroundColor: ['#FFD700', '#008000', '#FF0000'],
-              hoverBackgroundColor: ['#FFD700', '#008000', '#FF0000'],
-              borderWidth: 1,
-              hoverOffset: 4,
-            },
-          ],
-        };
+      const processedChartData = items.reduce((acc, item) => {
+        const tipoInfo = getTipoDocumentoInfo(item.tipo_doc);
+        if (!acc[item.tipo_doc]) {
+          acc[item.tipo_doc] = { tipoDoc: tipoInfo.text, count: 0, color: tipoInfo.color };
+        }
+        acc[item.tipo_doc].count += 1;
+        return acc;
+      }, {});
 
-        setLineData(processedLineData);
-        setBarData(processedBarData);
-        setPieData(processedPieData);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
+      setLineData(processedLineData);
+      setBarData(processedBarData);
+      setPieData(processedPieData);
+      setChartData(Object.values(processedChartData));
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
 
+  useEffect(() => {
     fetchData();
   }, []);
 
@@ -244,6 +290,10 @@ const Dashboard = () => {
     },
   };
 
+  const handleChartClick = () => {
+    setSecondModalIsOpen(true);
+  };
+
   return (
     <div className="dashboard-container">
       <h2>Visão Geral</h2>
@@ -282,7 +332,7 @@ const Dashboard = () => {
           <h3>Documentos atualizados</h3>
           {barData && <Bar data={barData} options={options} />}
         </div>
-        <div className="chart-item">
+        <div className="chart-item" onClick={() => setPieModalIsOpen(true)}>
           <h3>Gráfico de Pizza</h3>
           {pieData && <Pie data={pieData} options={options} />}
         </div>
@@ -334,9 +384,9 @@ const Dashboard = () => {
             <label htmlFor="filtrarPeriodoPor">Filtrar período por:</label>
             <div className="input-dash-icon">
               <select id="filtrarPeriodoPor" name="filtrarPeriodoPor">
-              <option value="dataEmissao">Data de Emissão</option>
-              <option value="dataTransacao">Data de Transação</option>
-            </select>
+                <option value="dataEmissao">Data de Emissão</option>
+                <option value="dataTransacao">Data de Transação</option>
+              </select>
             </div>
           </div>
           <div className="input-item-dash-rec">
@@ -379,7 +429,7 @@ const Dashboard = () => {
             </div>
           </div>
           <div className="input-item-dash-config">
-          <label htmlFor="graficosDash">Gráficos:</label>
+            <label htmlFor="graficosDash">Gráficos:</label>
             <div className="multi-select" ref={graficosDropdownRef}>
               <div className="multi-select__control" onClick={() => setShowGraficosDropdown(!showGraficosDropdown)}>
                 <span>{graficosSelecao.length > 0 ? graficosSelecao.join(', ') : 'Selecionar...'}</span>
@@ -410,7 +460,7 @@ const Dashboard = () => {
             </div>
           </div>
           <div className="input-item-dash-config">
-          <label htmlFor="bigNumbers">Big Numbers:</label>
+            <label htmlFor="bigNumbers">Big Numbers:</label>
             <div className="multi-select" ref={bigNumbersDropdownRef}>
               <div className="multi-select__control" onClick={() => setShowBigNumbersDropdown(!showBigNumbersDropdown)}>
                 <span>{bigNumbersSelecao.length > 0 ? bigNumbersSelecao.join(', ') : 'Selecionar...'}</span>
@@ -441,6 +491,92 @@ const Dashboard = () => {
             </div>
           </div>
           <button className="save-button-config">Salvar</button>
+        </div>
+      </Modal>
+      <Modal
+        isOpen={pieModalIsOpen}
+        onRequestClose={() => setPieModalIsOpen(false)}
+        className="config-modal-content-graph"
+        overlayClassName="config-modal-backdrop-graph"
+      >
+        <div className="modal-config-graph">
+          <h2 className='title-config-graph'>Gráfico de Pizza Ampliado</h2>
+          <button className="close-button-dash-graph" onClick={() => setPieModalIsOpen(false)}>&times;</button>
+        </div>
+        <div className="chart-container-graph" onClick={handleChartClick}>
+          {pieData && <Pie data={pieData} options={options} />}
+        </div>
+      </Modal>
+      <Modal
+        isOpen={secondModalIsOpen}
+        onRequestClose={() => setSecondModalIsOpen(false)}
+        className="config-modal-content-second-graph"
+        overlayClassName="config-modal-backdrop-second-graph"
+      >
+        <div className="modal-config-second-graph">
+          <h2 className='title-config-second-graph'>Gráfico de Tipo de Documento</h2>
+          <button className="close-button-dash-second-graph" onClick={() => setSecondModalIsOpen(false)}>&times;</button>
+        </div>
+        <div className="chart-container-second-graph">
+          <ApexCharts
+            options={{
+              chart: {
+                type: 'radialBar',
+              },
+              plotOptions: {
+                radialBar: {
+                  track: {
+                    background: '#fff',
+                    strokeWidth: '50%',
+                  },
+                  dataLabels: {
+                    total: {
+                      show: false,
+                    },
+                    value: {
+                      show: true,
+                      formatter: (val) => (val / 100) * 100,
+                      offsetY: 12,
+                      color: '#333',
+                      fontSize: '20px',
+                    },
+                  },
+                },
+              },
+              legend: {
+                show: true,
+                position: 'bottom',
+                horizontalAlign: 'center',
+                fontSize: '13px',
+                labels: {
+                  colors: '#333',
+                },
+              },
+              tooltip: {
+                enabled: true,
+                shared: true,
+                followCursor: false,
+                intersect: false,
+                style: {
+                  fontSize: '14px',
+                },
+                onDatasetHover: {
+                  highlightDataSeries: true,
+                },
+                x: {
+                  show: true,
+                },
+                marker: {
+                  show: true,
+                },
+              },
+              labels: chartData.map(item => item.tipoDoc),
+            }}
+            series={chartData.map(item => item.count)}
+            type="radialBar"
+            height={450}
+            width={450}
+          />
         </div>
       </Modal>
     </div>
